c88fd43b3b86919d28ac0404bdbe95e6713c2fe6
Om-2020 <omprakashbharti1123@gmail.com>
2023-07-07 00:34:43 +0530

    initial commit

---
+++ src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import { Box, Heading, HStack, Stack, VStack } from '@chakra-ui/react';
+import React from 'react';
+import {
+  TiSocialYoutubeCircular,
+  TiSocialInstagramCircular,
+} from 'react-icons/ti';
+import { DiGithub } from 'react-icons/di';
+const Footer = () => {
+  return (
+    <Box padding={'4'} bg="blackAlpha.900" minH={'10vh'}>
+      <Stack direction={['column', 'row']}>
+        <VStack alignItems={['center', 'flex-start']} width="full">
+          <Heading children="All Rights Reserved" color={'white'} />
+          <Heading
+            fontFamily={'body'}
+            size="sm"
+            children="@Om Prakash Bharti"
+            color={'red.400'}
+          />
+        </VStack>
+
+        <HStack
+          spacing={['2', '10']}
+          justifyContent="center"
+          color={'white'}
+          fontSize="50"
+        >
+          <a href="https://www.youtube.com/channel/UCZHmvPgrgF1XiyFTDQ5B-DA" target={'blank'}>
+            <TiSocialYoutubeCircular />
+          </a>
+          <a href="https://instagram.com/_om_bharti_" target={'blank'}>
+            <TiSocialInstagramCircular />
+          </a>
+          <a href="https://github.com/Om-2020" target={'blank'}>
+            <DiGithub />
+          </a>
+        </HStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
